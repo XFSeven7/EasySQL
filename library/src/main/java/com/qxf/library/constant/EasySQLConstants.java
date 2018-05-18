b948954441953bc195d05d59e5ea2e1ffbf61523
qixuefeng <xfseven7@foxmail.com>
2018-05-18 15:59:20 +0800

    数据库常量字段

---
+++ library/src/main/java/com/qxf/library/constant/EasySQLConstants.java
@@ -0,0 +1,39 @@
+package com.qxf.library.constant;
+
+public class EasySQLConstants {
+
+    // ------------------- 数据类型 ----------------------
+
+    public static final String TYPE_BYTE = "byte";
+    public static final String TYPE_LONG = "long";
+    public static final String TYPE_FLOAT = "float";
+    public static final String TYPE_SHORT = "short";
+    public static final String TYPE_BYTE_ARR = "class [B";
+    public static final String TYPE_DOUBLE = "double";
+    public static final String TYPE_STRING = "class java.lang.String";
+    public static final String TYPE_BOOLEAN = "boolean";
+    public static final String TYPE_INT = "int";
+
+
+
+    // ------------------- sql字段（对应上面对数据类型） -----------------------------------
+
+    public static final String SQL_BYTE = "byte";
+    public static final String SQL_LONG = "long";
+    public static final String SQL_TEXT = "text";
+    public static final String SQL_SHORT = "short";
+    public static final String SQL_INT = "int";
+    public static final String SQL_FLOAT = "float";
+    public static final String SQL_DOUBLE = "double";
+    public static final String SQL_BOOLEAN = "boolean";
+    public static final String SQL_BYTE_ARR = "blob";
+
+    public static final String SQL_TABLE = "create table";
+
+    public static final String SQL_LEFT = "(";
+    public static final String SQL_RIGHT = ")";
+
+    public static final String SQL_SPACE = " ";
+    public static final String SQL_COMMA = ",";
+
+}
