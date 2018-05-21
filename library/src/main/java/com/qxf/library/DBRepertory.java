2ee74aa31f7193e0ff6ad684b71c6112a021e3e4
qixuefeng <xfseven7@foxmail.com>
2018-05-21 09:44:08 +0800

    存储数据库的仓库

---
+++ library/src/main/java/com/qxf/library/DBRepertory.java
@@ -0,0 +1,81 @@
+package com.qxf.library;
+
+import com.qxf.library.db.DBHelper;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class DBRepertory {
+
+    private static final String TAG = "DBRepertory";
+
+    private static DBRepertory instance;
+
+    private HashMap<String, DBHelper> dbList = new HashMap<>();
+
+    private DBRepertory() {
+    }
+
+    public static DBRepertory getInstance() {
+        if (instance == null) {
+            synchronized (DBRepertory.class) {
+                if (instance == null) {
+                    instance = new DBRepertory();
+                }
+            }
+        }
+        return instance;
+    }
+
+    /**
+     * 创建数据库
+     *
+     * @param dbName 数据库名字
+     */
+    public void add(String dbName) {
+
+        if (check(dbName)) {
+            return;
+        }
+
+        DBHelper dbHelper = new DBHelper(EasySQL.context(), dbName, dbList.size());
+        dbHelper.getWritableDatabase();
+        dbList.put(dbName, dbHelper);
+
+    }
+
+    /**
+     * 获取指定名字的数据库
+     *
+     * @param dbName 数据库名字
+     * @return the db
+     */
+    public DBHelper get(String dbName) {
+        if (check(dbName)) {
+            return dbList.get(dbName);
+        }
+        throw new NullPointerException("该数据库不存在");
+    }
+
+    /**
+     * 检查该数据库是否已存在
+     *
+     * @param dbName 数据名
+     * @return 存在为true, 不存在为false
+     */
+    private boolean check(String dbName) {
+        Set<String> strings = dbList.keySet();
+        return strings.contains(dbName);
+    }
+
+    /**
+     * 获取所有数据库名字
+     *
+     * @return
+     */
+    public HashSet<String> listName() {
+        return (HashSet<String>) dbList.keySet();
+    }
+
+}
