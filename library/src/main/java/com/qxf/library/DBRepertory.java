083ed731056d57a46f9a10ac839aec460cb60624
qixuefeng <xfseven7@foxmail.com>
2018-05-28 15:03:27 +0800

    部分操作优化

--- library/src/main/java/com/qxf/library/DBRepertory.java
+++ library/src/main/java/com/qxf/library/DBRepertory.java
@@ -18,16 +18,13 @@ public class DBRepertory {
 
     private static DBRepertory instance;
 
+    private Context context;
+
     private HashMap<String, DBHelper> dbList = new HashMap<>();
 
     private DBRepertory(Context context) {
-
-        Set<String> stringSet = getDB();
-
-        for (String s : stringSet) {
-            dbList.put(s, new DBHelper(context, s));
-        }
-
+        this.context = context;
+        fresh();
     }
 
     public static DBRepertory getInstance(Context context) {
@@ -42,12 +39,23 @@ public class DBRepertory {
     }
 
     /**
+     * 刷新
+     */
+    private void fresh() {
+        dbList.clear();
+        Set<String> stringSet = getDB();
+        for (String s : stringSet) {
+            dbList.put(s, new DBHelper(context, s));
+        }
+    }
+
+    /**
      * 创建数据库
      *
      * @param dbName 数据库名字
      */
     public void add(String dbName, Context context) {
-
+        fresh();
         if (TextUtils.isEmpty(dbName))
             dbName = EasySQLConstants.SQL_DEFAULT_NAME + EasySQLConstants.SQL_END_TABLE;
         if (!dbName.endsWith(EasySQLConstants.SQL_END_TABLE))
@@ -70,6 +78,7 @@ public class DBRepertory {
      * @return 是否删除成功
      */
     public boolean delete(String dbName, Context context) {
+        fresh();
         dbName += EasySQLConstants.SQL_END_TABLE;
         dbList.remove(dbName);
         removeDB(dbName);
@@ -83,11 +92,13 @@ public class DBRepertory {
      * @return the db
      */
     public DBHelper get(String dbName) {
+        fresh();
         if (TextUtils.isEmpty(dbName)) {
             dbName = EasySQLConstants.SQL_DEFAULT_NAME;
         }
         dbName += EasySQLConstants.SQL_END_TABLE;
-        if (check(dbName)) {
+
+        if (check(dbName, getDB())) {
             return dbList.get(dbName);
         }
         throw new NullPointerException("该数据库不存在");
@@ -102,6 +113,17 @@ public class DBRepertory {
     private boolean check(String dbName) {
         Set<String> strings = dbList.keySet();
         return strings.contains(dbName);
+    }
+
+    /**
+     * 检查该数据库是否已存在
+     *
+     * @param dbName     数据名
+     * @param dbNameList 数据库名字列表
+     * @return 存在为true, 不存在为false
+     */
+    private boolean check(String dbName, Set<String> dbNameList) {
+        return dbNameList.contains(dbName);
     }
 
     /**
@@ -135,7 +157,7 @@ public class DBRepertory {
     /**
      * 删除数据库
      *
-     * @param dbName
+     * @param dbName 数据库名字
      */
     private void removeDB(String dbName) {
         Set<String> db = getDB();
